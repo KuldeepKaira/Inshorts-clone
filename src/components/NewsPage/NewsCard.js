440f7921a5a37fb9242202d4f972665faa7be0af
KuldeepKaira <kkaira218@gmail.com>
2022-01-11 12:11:04 +0530

    Entered some more Data

--- src/components/NewsPage/NewsCard.js
+++ src/components/NewsPage/NewsCard.js
@@ -1,18 +1,6 @@
 import React from "react";
 import classes from "./NewsCard.module.css";
 
-const DUMMY_DATA = {
-  title:
-    "Akshay Kumar Bell Bottom screened at world highest mobile theatre in Ladakh",
-  author: "Sakshita Khosla",
-  description:
-    "Actor Akshay Kumar on Sunday said his recent release Bell Bottom was screened at a travelling cinema hall in Ladakh, touted as the worlds highest mobile theatre. [It] makes my heart swell with pride, said Akshay, sharing a picture of the theatre on Twitter. He added, At an altitude of 11,562 feet, the theatre can operate at -28 °C.",
-  url: "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2021/08_aug/29_sun/img_1630255412572_833.jpg",
-  timestamp: "2021-08-29T16:50:33.000Z",
-  link: "https://www.hindustantimes.com/entertainment/bollywood/akshay-kumar-s-bellbottom-screened-at-world-s-highest-mobile-theatre-in-ladakh-makes-my-heart-swell-with-pride-101630246753605-amp.html?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
-  publisher: "Hindustan Times",
-};
-
 const timeStamp = (time) => {
   const months = [
     "Jan",
@@ -55,27 +43,23 @@ const timeStamp = (time) => {
   return timeoutput;
 };
 
-const NewsCard = () => {
+const NewsCard = (props) => {
   return (
     <div className={classes.newsCard}>
       <div className={classes.newsImgDiv}>
-        <img
-          src={DUMMY_DATA.url}
-          alt="news image"
-          className={classes.newsImg}
-        />
+        <img src={props.url} alt="news image" className={classes.newsImg} />
       </div>
       <div className={classes.newsContent}>
-        <p className={classes.title}>{DUMMY_DATA.title}</p>
+        <p className={classes.title}>{props.title}</p>
         <p className={classes.author}>
           <span className={classes.short}>short</span>
-          {` by ${DUMMY_DATA.author}/${timeStamp(DUMMY_DATA.timestamp)}`}
+          {` by ${props.author}/${timeStamp(props.timestamp)}`}
         </p>
-        <p className={classes.description}>{DUMMY_DATA.description}</p>
+        <p className={classes.description}>{props.description}</p>
         <p className={classes.linkInfo}>
           read more at
-          <a className={classes.link} href={DUMMY_DATA.url}>
-            {` ${DUMMY_DATA.publisher}`}
+          <a className={classes.link} href={props.url}>
+            {` ${props.publisher}`}
           </a>
         </p>
       </div>
--- src/components/NewsPage/NewsPage.js
+++ src/components/NewsPage/NewsPage.js
@@ -2,15 +2,30 @@ import React from "react";
 import AddBar from "./AddBar.js";
 import classes from "./NewsPage.module.css";
 import NewsCard from "./NewsCard.js";
+import { data } from "./Data.js";
 
 const NewsPage = (props) => {
+  const newsData = data;
+  const display = newsData.map((news) => {
+    return (
+      <NewsCard
+        key={Math.random() * 100}
+        title={news.title}
+        author={news.author}
+        description={news.description}
+        url={news.url}
+        timestamp={news.timestamp}
+        link={news.link}
+        publisher={news.publisher}
+      />
+    );
+  });
+  console.log(display);
+
   return (
     <div className={classes.newsContainer}>
       <AddBar />
-      <NewsCard />
-      <NewsCard />
-      <NewsCard />
-      <NewsCard />
+      {display}
     </div>
   );
 };
